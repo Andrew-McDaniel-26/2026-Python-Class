6e9aaca8f5d51b931dfaac93f2400027bc22cb29
Andrew-McDaniel-26 <mcdanielandrew0528@gmail.com>
2026-09-03 11:42:48 -0500

    Add files via upload

--- Python class Lab 2/Andrew McDaniel lab 2 star.py
+++ Python class Lab 2/Andrew McDaniel lab 2 star.py
@@ -1,10 +1,18 @@
 import turtle
+#Andrew McDaniel, Python Class Lab 2, 09/03/26, this program draws a star using turtle graphics
 
+#this is the function that draws the star
 def drawstar(myTurtle, sideLength):
     myTurtle.color("blue")
     for i in range(5):
         myTurtle.forward(sideLength)
         myTurtle.right(144)
 
+# initialize the turtle and call the function to draw the star
 t = turtle.Turtle()
 drawstar(t, 100)
+
+# this is to keep the turtle graphics window open until the user closes it
+x = ""
+input(x)
+print(x)
--- Python class Lab 2/Andrew Mcdaniel lab 2 flower.py
+++ Python class Lab 2/Andrew Mcdaniel lab 2 flower.py
@@ -1,5 +1,9 @@
-import turtle 
 
+
+import turtle 
+#Andrew McDaniel, Python Class Lab 2, 09/03/26, this program draws a flower with a stem using turtle graphics
+
+#this is the function that draws the flower and stem
 def drawflower(myTurtle, petalLength, stemLength):
 	myTurtle.color("green", "blue")
 	myTurtle.begin_fill()
@@ -40,10 +44,11 @@ def drawflower(myTurtle, petalLength, stemLength):
 	myTurtle.right(54)
 	myTurtle.forward(stemLength)
 
-
+# initialize the turtle and call the function to draw the flower
 t = turtle.Turtle()
-
 drawflower(t, 50, 100)
+
+# this is to keep the turtle graphics window open until the user closes it
 x = ""
 input(x)
 print(x)
